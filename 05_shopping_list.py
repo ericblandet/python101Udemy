46703df025312d10969f91988675fd559166de7f
eric <eric.blandet@gmail.com>
2022-11-06 19:54:48 +0100

    shopping_list_ok

---
+++ 05_shopping_list.py
@@ -0,0 +1,43 @@
+import sys
+
+shopping_list = []
+instruction="---------------------\n1:Ajouter un élément\n2:Retirer un élément\n3:Afficher la liste\n4:Vider la liste\n5:Quitter\nQuel est ton choix: "
+choice=input(instruction)
+
+while choice != "5":
+  if not (choice.isdigit()) or int(choice) > 5:
+    print("Please provide a number between 1 and 5")
+    choice=input(instruction)
+  elif int(choice) == 1:
+    to_add = input("Quel élément ajouter?")
+    shopping_list.append(to_add)
+    print(shopping_list)
+    print(f"L'élément {to_add} a été ajouté")
+    choice=input(instruction)
+  elif int(choice) == 2:
+    to_remove=input("Quel élément retirer?")
+    if to_remove not in shopping_list:
+      choice=input(instruction)
+    else:
+      index = shopping_list.index(to_remove)
+      shopping_list.pop(index)
+      print(f"L'élément {to_remove} a été retiré")
+      choice=input(instruction)
+  elif int(choice) == 3:
+    if len(shopping_list) ==0:
+      print("nothing to display")
+    for i in shopping_list:
+      print(f"{shopping_list.index(i)+1} : {i}")
+    choice=input(instruction)
+  elif int(choice) == 4:
+    shopping_list.clear()
+    print("La liste a été vidée")
+    choice=input(instruction)
+  elif int(choice) == 5:
+    break
+  else :
+    print("something went wrong !")
+    choice=input(instruction)
+  
+print("ciao !")
+sys.exit()
